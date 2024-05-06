7273e90357846335a4236d31409d16e9da957c24
Ebraheem Alhetari <108173819+Hetari@users.noreply.github.com>
2024-05-06 21:07:19 +0300

    Time: 71 ms (5.41%) | Memory: 50.9 MB (80.29%) - LeetSync

---
+++ 2796-allow-one-function-call/allow-one-function-call.ts
@@ -0,0 +1,21 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let runs: boolean = true;
+    return function (...args) {
+        if (!runs){
+            return undefined;
+        }
+        runs = false;
+        return fn(...args)
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
