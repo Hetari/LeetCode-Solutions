a915391b7a5d3250e8808e7a73858276a281a378
Ebraheem Alhetari <108173819+Hetari@users.noreply.github.com>
2024-05-09 16:36:38 +0300

    Time: 316 ms (39.97%) | Memory: 88.3 MB (64.21%) - LeetSync

---
+++ 2731-memoize/memoize.ts
@@ -0,0 +1,24 @@
+type Fn = (...params: number[]) => number
+
+function memoize(fn: Fn): Fn {
+  const cache: { [key: string]: number } = {};
+  return function (...args) {
+    const key = JSON.stringify(args);
+    if (key in cache) {
+      return cache[key];
+    }
+    cache[key] = fn(...args);
+    return cache[key];
+  };
+}
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
